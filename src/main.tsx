14ece31e65f5500931a85b74fdd394fee71e0dc1
ayubhussainis-dot <ayubhussainis@gmail.com>
2026-09-24 13:50:31 +0530

    Initialize G-BOT application entry point

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+// G-BOT Application Entry Point
+// Written by GemInEye
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GBotIndex from './pages/Index';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GBotIndex />
+  </React.StrictMode>
+);
