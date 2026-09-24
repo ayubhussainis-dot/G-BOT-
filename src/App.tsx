5c0cab1777097c5f4fe3110a0eaca4d4eb618e53
ayubhussainis-dot <ayubhussainis@gmail.com>
2026-09-24 20:34:35 +0530

    Refactor App component to include TelemetryDashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import Index from "./pages/Index";
+// App.tsx
+import React from 'react';
+import { TelemetryDashboard } from './TelemetryDashboard';
 
-export default function App() {
-  return <Index />;
+export function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <TelemetryDashboard />
+    </div>
+  );
 }
+
+export default App;
